5af6ae36c07d4202198a4f5c14ddd6b714f73cdb
manisha <elmayadav988@gmail.com>
2025-09-30 15:59:46 +0530

    Initial commit -Deepawali page with Adstera & PropellerAds,audio,fireworks,share buttons

---
+++ README.md
@@ -0,0 +1,2 @@
+# Mata
+Mata 
---
+++ sw.js
@@ -0,0 +1,6 @@
+self.options = {
+    "domain": "shoukigaigoors.net",
+    "zoneId": 9953686
+}
+self.lary = ""
+importScripts('https://shoukigaigoors.net/act/files/service-worker.min.js?r=sw')
